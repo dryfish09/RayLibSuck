cee44231d924892977f9f8d8c12327586ca937b4
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-20 06:15:13 +0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include "raylib.h"
+
+int main() {
+    constexpr int windowX = 800;
+    constexpr int windowY = 700;
+    constexpr float ballRange = 60;
+    // init windows
+    InitWindow(windowX, windowY, "Ball's tiny world 2D");
+    // set target fps
+    SetTargetFPS(60);
+    // create a ball:
+    Vector2 ballPos = {30, 40};
+    // ishowspeed, lol, it's speed
+    float speed = 4.0f; // chill
+    while (!WindowShouldClose()) {
+        // move ball with arrows
+        if (IsKeyDown(KEY_RIGHT)) {
+            // right: use <entityPos>.x += speed;
+            ballPos.x += speed;
+        }
+        if (IsKeyDown(KEY_LEFT)) {
+            // left: like right, but is <entityPos>.x -= speed;
+            ballPos.x -= speed;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            // down: <entityPos>.y += speed;
+            ballPos.y += speed;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            ballPos.y -= speed;
+        }
+        // prevent player go out of screen
+        if (ballPos.x - ballRange <= 0) {
+            ballPos.x = ballRange;
+        }
+        if (ballPos.x + ballRange >= windowX) {
+            ballPos.x = windowX - ballRange;
+        }
+        if (ballPos.y - ballRange <= 0) {
+            ballPos.y = ballRange;
+        }
+        if (ballPos.y + ballRange >= windowY) {
+            ballPos.y = windowY - ballRange;
+        }
+        // draw it!
+        BeginDrawing();
+        ClearBackground(GREEN);
+        // draw a circle
+        DrawCircleV(ballPos, ballRange, GOLD);
+        DrawText("Use arrow keys to move!", 20, 20, 20, BLACK);
+        EndDrawing();
+    }
+    // only close window when it should close
+    CloseWindow();
+    return 0;
+}
