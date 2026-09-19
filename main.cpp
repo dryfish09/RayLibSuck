eb915e7c3f29fb48ce2fc625f8189414511261eb
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-19 19:09:52 +0700

    feat: improve wall logic, refactor code for cleaner

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "raylib.h"
 
 int main() {
+    int windowX = 800;
+    int windowY = 700;
+    float ballRange = 60;
     // init windows
-    InitWindow(800, 700, "First Raylib program");
+    InitWindow(windowX, windowY, "Ball's tiny world 2D");
     // set target fps, 30 is good for mobile compiler
     SetTargetFPS(30);
     // create a ball:
@@ -10,7 +13,7 @@ int main() {
     // ishowspeed, lol, it's speed
     float speed = 3.5f; // chill
     // wall to prevent player go out of screen, try to use constexpr
-    constexpr int wall = 5;
+    constexpr int wall = 10;
     while (!WindowShouldClose()) {
         // move ball with arrows
         // IsKeyPressed for one move at a time
@@ -31,25 +34,25 @@ int main() {
         }
         // wall
         if (ballPos.x - wall <= 0) {
-            ballPos.x = wall;
+            ballPos.x = ballRange;
         }
         if (ballPos.x + wall >= 800) {
-            ballPos.x = wall;
+            ballPos.x = wall + ballRange;
         }
         if (ballPos.y - wall <= 0) {
-            ballPos.y = wall;
+            ballPos.y = ballRange;
         }
         if (ballPos.y + wall >= 700) {
-            ballPos.y = wall;
+            ballPos.y = ballRange;
         }
         // draw it!
         BeginDrawing();
         ClearBackground(WHITE);
         // draw a circle
-        DrawCircleV(ballPos, 60, GREEN);
+        DrawCircleV(ballPos, ballRange, GREEN);
         DrawText("Use Arrow keys to move!", 20, 20, 20, BLACK);
         EndDrawing();
     }
     CloseWindow();
-    return 10; // i like 10, not 0
+    return 0;
 }
