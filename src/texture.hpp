42f1bf1b5e446dc96a196e3a7896f368ff8d6cca
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-21 20:50:04 +0700

    log: log error when can't load texture

--- src/texture.hpp
+++ src/texture.hpp
@@ -4,6 +4,10 @@ namespace texture {
     inline Texture2D flowerTexture;
     inline void loadTexture() {
         flowerTexture = LoadTexture("src/assets/texture/flower.png");
+        // make sure texture loaded by check it
+        if (flowerTexture.id == 0) {
+            TraceLog(LOG_ERROR, "texture is not available or can't be loaded.");
+        }
     }
     inline void unloadTexture() {
         UnloadTexture(flowerTexture);
