88f8bf0475634e47973e525b5d510c4e2305c807
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-22 16:43:48 +0700

    fix: remove delta time because it unecessary and change `constexpr` to `const` (#32)
    …

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include "./texture.hpp"
 
 int main() {
-    constexpr int windowX = 800;
-    constexpr int windowY = 700;
-    constexpr float ballRadius = 9.5f;
+    const int windowX = 800;
+    const int windowY = 700;
+    const float ballRadius = 9.5f;
     // init windows
     InitWindow(windowX, windowY, "Ball's tiny world 2D");
     // init sound:
@@ -21,7 +21,7 @@ int main() {
     Vector2 flowerPos = {0, 0};
     Vector2 flowerPos2 = {0, 0};
     Vector2 flowerPos3 = {0, 0};
-    constexpr int flowerRadius = 20;
+    const int flowerRadius = 20;
     flowerPos.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
     flowerPos.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
     flowerPos2.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
@@ -31,54 +31,53 @@ int main() {
     // ishowspeed, lol, it's speed
     float speed = 5.3f; // not chill
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime();
         // move ball with arrows
         if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) {
             speed = 5.3f;
             // right: use <entityPos>.x += speed;
-            ballPos.x += speed * deltaTime;
+            ballPos.x += speed;
             // play sound
             sound::playFootstepSound();
         }
         else if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) {
             // left: like right, but is <entityPos>.x -= speed;
             speed = 5.3f;
-            ballPos.x -= speed * deltaTime;
+            ballPos.x -= speed;
             sound::playFootstepSound();
         }
         else if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) {
             // down: <entityPos>.y += speed;
             speed = 5.3f;
-            ballPos.y += speed * deltaTime;
+            ballPos.y += speed;
             sound::playFootstepSound();
         }
         else if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) {
             speed = 5.3f;
-            ballPos.y -= speed * deltaTime;
+            ballPos.y -= speed;
             sound::playFootstepSound();
         }
         // player will move slower when player hold to move
         else if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
             speed = 3.5f;
             // right: use <entityPos>.x += speed;
-            ballPos.x += speed * deltaTime;
+            ballPos.x += speed;;
             sound::playRunSound();
         }
         else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
             speed = 3.5f;
             // left: like right, but is <entityPos>.x -= speed;
-            ballPos.x -= speed * deltaTime;
+            ballPos.x -= speed;
             sound::playRunSound();
         }
         else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
             speed = 3.5f;
             // down: <entityPos>.y += speed;
-            ballPos.y += speed * deltaTime;
+            ballPos.y += speed;
             sound::playRunSound();
         }
         else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
             speed = 3.5f;
-            ballPos.y -= speed * deltaTime;
+            ballPos.y -= speed;
             sound::playRunSound();
         }
         // prevent player go out of screen
