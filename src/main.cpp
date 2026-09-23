87d3d1246963ea30584da9175cbcfe45b717e13d
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-23 20:56:58 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include "./sound.hpp"
 #include "./texture.hpp"
 #include "./logic.hpp"
+#include "./initializer.hpp"
 
 int main() {
+    init();
     // set target fps
     SetTargetFPS(60);
     insertLogic();
+    closeAndUnload();
     return 0;
 }
 
