255bf43e8374e3987458cf08a8231a0897b5b7c4
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-23 20:46:40 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,6 @@
 #include "./logic.hpp"
 
 int main() {
-    // init windows
-    InitWindow(windowX, windowY, "Ball's tiny world 2D");
-    // init sound:
-    InitAudioDevice();
-    sound::loadFootstepSound();
-    sound::loadRunSound();
-    //load texture
-    texture::loadTexture();
     // set target fps
     SetTargetFPS(60);
     insertLogic();
