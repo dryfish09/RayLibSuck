c91ba35daa246765b757faecccc347eeaab1de23
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 12:35:38 +0700

    Create texture.cpp

---
+++ src/game/texture/texture.cpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "raylib.h"
+namespace texture {
+    Texture2D flowerTexture;
+    Texture2D bg;
+    void drawBackground(Texture2D tex) {
+    DrawTexturePro(tex,
+        (Rectangle){ 0, 0, (float)tex.width, (float)tex.height },
+        (Rectangle){ 0, 0, (float)GetScreenWidth(), (float)GetScreenHeight() },
+        (Vector2){ 0, 0 }, 0.0f, WHITE);
+    }
+    void loadTexture() {
+        flowerTexture = LoadTexture("src/assets/texture/flower.png");
+        // make sure texture loaded by check it
+        if (flowerTexture.id == 0) {
+            TraceLog(LOG_ERROR, "texture is not available or can't be loaded.");
+        }
+    }
+    void loadBg() {
+        bg = LoadTexture("src/assets/texture/bg.png");
+    }
+    void unloadBg() {
+        UnloadTexture(bg);
+    }
+    void unloadTexture() {
+        UnloadTexture(flowerTexture);
+    }
+} // namespace
