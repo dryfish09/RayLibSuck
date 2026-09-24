6894009837001cfa9218f459cc88457489625fc4
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 13:48:15 +0700

    Create sound.cpp

---
+++ src/game/sound/sound.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "raylib.h"
+
+namespace sound {
+
+    Sound footstep;
+    Sound run;
+
+    void loadFootstepSound() {
+        footstep = LoadSound("src/assets/sound/walkSound.wav");
+    }
+    void loadRunSound() {
+        run = LoadSound("src/assets/sound/run.wav");
+    }
+    void playRunSound() {
+        if (!IsSoundPlaying(run) && !IsSoundPlaying(footstep)) {
+            PlaySound(run);
+        }
+    }
+
+    void playFootstepSound() {
+        if (!IsSoundPlaying(run) && !IsSoundPlaying(footstep)) {
+            PlaySound(footstep);
+        }
+    }
+
+    void unloadDeclaredSound() {
+        UnloadSound(footstep);
+        UnloadSound(run);
+    }
+
+}
