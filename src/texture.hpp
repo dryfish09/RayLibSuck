791553e4cae2e623a58be0ca38b5504c275a23a0
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 11:13:44 +0700

    Update texture.hpp

--- src/texture.hpp
+++ src/texture.hpp
@@ -2,12 +2,22 @@
 #include "raylib.h"
 namespace texture {
     inline Texture2D flowerTexture;
+    inline Texture2D bg;
+    inline void DrawBackground(Texture2D tex) {
+    DrawTexturePro(tex,
+        (Rectangle){ 0, 0, (float)tex.width, (float)tex.height },
+        (Rectangle){ 0, 0, (float)GetScreenWidth(), (float)GetScreenHeight() },
+        (Vector2){ 0, 0 }, 0.0f, WHITE);
+    }
     inline void loadTexture() {
         flowerTexture = LoadTexture("src/assets/texture/flower.png");
         // make sure texture loaded by check it
         if (flowerTexture.id == 0) {
             TraceLog(LOG_ERROR, "texture is not available or can't be loaded.");
         }
+    }
+    inline void loadBg() {
+        bg = LoadTexture("src/assets/texture/bg.png");
     }
     inline void unloadTexture() {
         UnloadTexture(flowerTexture);
