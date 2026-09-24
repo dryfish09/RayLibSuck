5dab8d0656b4414cccc2b5b3c32fc2a03fbf9b90
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 12:29:42 +0700

    Update and rename var.hpp to var.h

---
+++ src/game/var.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "raylib.h"
+
+extern int windowX;
+extern int windowY;
--- src/game/var.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "raylib.h"
-
-extern int windowX = 800;
-extern const int windowY = 700;
