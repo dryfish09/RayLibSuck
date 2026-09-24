9836f055abef61bccf4f88a2484f1afada5384cc
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 13:50:13 +0700

    Create sound.h

---
+++ src/game/sound/sound.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "raylib.h"
+
+namespace sound {
+
+    extern Sound footstep;
+    extern Sound run;
+
+    void loadFootstepSound();
+    void loadRunSound();
+    void playRunSound();
+
+    void playFootstepSound();
+
+    void unloadDeclaredSound();
+}
