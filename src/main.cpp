9fc5e76df175de318022db4843b1da790ce95e1b
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-24 14:35:09 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "raylib.h"
-#include "./game/sound/sound.h"
-#include "./game/texture/texture.hpp"
-#include "./game/logic/logic.h"
-#include "./game/init/init.h"
+#include "game/sound/sound.h"
+#include "game/texture/texture.hpp"
+#include "game/logic/logic.h"
+#include "game/init/init.h"
 
 int main() {
     init();
