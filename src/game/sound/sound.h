a38aa07b7d84af3d6bcef5871104c9ef03d88233
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-25 20:16:37 +0700

    Update sound.h

--- src/game/sound/sound.h
+++ src/game/sound/sound.h
@@ -4,12 +4,14 @@ namespace sound {
 
     extern Sound footstep;
     extern Sound run;
+    extern Music bgm;
 
     void loadFootstepSound();
+    void loadBgm();
     void loadRunSound();
     void playRunSound();
-
+    void playBgm();
     void playFootstepSound();
-
+    void unloadBgm();
     void unloadDeclaredSound();
 }
