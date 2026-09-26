58fada4d1842493bb578dc6735c1e421023366a5
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-26 09:51:13 +0700

    Update flower_spawner.h

--- src/game/spawn/flower/flower_spawner.h
+++ src/game/spawn/flower/flower_spawner.h
@@ -1,5 +1,12 @@
 #include "raylib.h"
 #include "flower_spawner.h"
 namespace spawn {
+    extern Vector2 flowerPos;
+    extern Vector2 flowerPos2;
+    extern Vector2 flowerPos3;
+    extern Vector2 flowerPos4;
+    extern Vector2 flowerPos5;
+    extern Vector2 flowerPos6;
+    extern const int flowerRadius;
     void spawnFlower();
 }
