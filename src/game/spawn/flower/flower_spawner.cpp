f74d3f8e516ca6950cfafc6ab32127652598117b
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-26 09:36:56 +0700

    Create flower_spawner.cpp

---
+++ src/game/spawn/flower/flower_spawner.cpp
@@ -0,0 +1,25 @@
+#include "raylib.h"
+#include "flower_spawner.h"
+namespace spawn {
+    void spawnFlower() {
+        Vector2 flowerPos = {0, 0};
+        Vector2 flowerPos2 = {0, 0};
+        Vector2 flowerPos3 = {0, 0};
+        Vector2 flowerPos4 = {0, 0};
+        Vector2 flowerPos5 = {0, 0};
+        Vector2 flowerPos6 = {0, 0};
+        const int flowerRadius = 20;
+        flowerPos.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+        flowerPos2.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos2.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+        flowerPos3.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos3.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+        flowerPos4.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos4.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+        flowerPos5.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos5.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+        flowerPos6.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
+        flowerPos6.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+    }
+}
