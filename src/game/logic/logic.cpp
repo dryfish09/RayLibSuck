c1741143c60e3f77ce6ef4e5d908f8e8a69fdb29
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-26 09:33:30 +0700

    Update logic.cpp

--- src/game/logic/logic.cpp
+++ src/game/logic/logic.cpp
@@ -3,29 +3,11 @@
 #include "game/var.h"
 #include "game/sound/sound.h"
 #include "game/texture/texture.h"
+#include "game/spawn/flower/flower_spawner.h"
 #include "./logic.h"
 inline const float ballRadius = 9.5f;
 void insertLogic() {
-    Vector2 ballPos = {30, 40};
-    Vector2 flowerPos = {0, 0};
-    Vector2 flowerPos2 = {0, 0};
-    Vector2 flowerPos3 = {0, 0};
-    Vector2 flowerPos4 = {0, 0};
-    Vector2 flowerPos5 = {0, 0};
-    Vector2 flowerPos6 = {0, 0};
-    const int flowerRadius = 20;
-    flowerPos.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
-    flowerPos2.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos2.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
-    flowerPos3.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos3.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
-    flowerPos4.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos4.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
-    flowerPos5.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos5.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
-    flowerPos6.x = (int)GetRandomValue(flowerRadius, windowX - flowerRadius);
-    flowerPos6.y = (int)GetRandomValue(flowerRadius, windowY - flowerRadius);
+    spawn::spawnFlower();
     // speed: 
     float speed = 5.5f;
     float runSpeed = 4.5f;
@@ -105,12 +87,12 @@ void insertLogic() {
         texture::drawBackground(texture::bg);
         // draw some circle
         DrawTextureV(texture::ball, ballPos, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos2, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos3, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos4, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos5, WHITE);
-        DrawTextureV(texture::flowerTexture, flowerPos6, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos2, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos3, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos4, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos5, WHITE);
+        DrawTextureV(texture::flowerTexture, spawn::flowerPos6, WHITE);
         DrawText("Use arrow keys or WASD to move!", 20, 20, 20, BLACK);
         EndDrawing();
     }
