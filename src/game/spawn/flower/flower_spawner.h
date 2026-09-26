b135eb3e51da8e82a5af81283922b9355ca8b05d
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-26 10:35:32 +0700

    refactor: move flower spawn logic into another file (#59)

---
+++ src/game/spawn/flower/flower_spawner.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "raylib.h"
+namespace spawn {
+    extern Vector2 flowerPos;
+    extern Vector2 flowerPos2;
+    extern Vector2 flowerPos3;
+    extern Vector2 flowerPos4;
+    extern Vector2 flowerPos5;
+    extern Vector2 flowerPos6;
+    extern const int flowerRadius;
+    void spawnFlower();
+}
